f90511a5f668ae419b4ffd09700762327474ffa0
Sophia Bell <sophbell87@gmail.com>
2020-04-25 19:01:26 +0100

    getting print statement test to pass in statement spec

--- spec/StatementSpec.js
+++ spec/StatementSpec.js
@@ -2,29 +2,27 @@
 
 describe ('Statement', function(){
 
+  let history = [{_credit: 50, _debit: 0, _date: '25/04/2020'}, {_credit: 0, _debit: 30, _date: '26/04/2020'}];
+  let fullStatement = new Statement(history);
+
   it('prints a blank statement when no transactions have been made', function() {
-    statement = new Statement(blank);
+    let statement = new Statement([]);
     spyOn(console, 'log').and.callThrough();
     expect(statement.printStatement()).toBe("date || credit || debit || balance\n");
   });
   
-  let deposit = jasmine.createSpyObj('transaction', {_credit: 50, _debit: 0, _date: '25/04/2020'});
-  let withdraw = jasmine.createSpyObj('transaction', {_credit: 0, _debit: 30, _date: '26/04/2020'});
-  let history = [deposit, withdraw];
-  let fullStatement = new Statement(history);
-
   it('adds amount to the balance', function() {
-    statement.calculateBalance(50, 0, 0);
-    expect(statement.getBalance()).toBe(50);
+    fullStatement.calculateBalance(50, 0, 0);
+    expect(fullStatement.getBalance()).toBe(50);
   })
 
   it('removes amount from the balance', function() {
-    statement.calculateBalance(0, 50, 0);
-    expect(statement.getBalance()).toBe(-50);
+    fullStatement.calculateBalance(0, 50, 0);
+    expect(fullStatement.getBalance()).toBe(-50);
   })
 
   it('prints a statment', function() {
-    expect(fullStatement.printStatement()).toBe("date || credit || debit || balance\n25/04/2020 || 50.00 || || 50.00\n26/04/2020 || || 30.00 || 22.00\n")
+    expect(fullStatement.printStatement()).toBe("date || credit || debit || balance\n26/04/2020 ||  || 30.00 || 20.00\n25/04/2020 || 50.00 ||  || 50.00\n")
   });
 
 });
